f0dc11cf6c3ce35bf208b252c5555f572ecb887c
wildafahaq <wildafahaq.com>
2024-05-03 21:41:26 +0700

    kedai-kopi

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika  hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.targer)) {
+    navbarNav.classList.remove("active");
+  }
+});
